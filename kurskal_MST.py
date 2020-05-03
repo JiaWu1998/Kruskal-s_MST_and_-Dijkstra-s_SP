111146a19efce1d669c7414cadc39ab988090ad4
James <Jiawu1998@github.com>
2020-05-03 14:23:31 -0500

    sorted edges for kurskal

--- kurskal_MST.py
+++ kurskal_MST.py
@@ -1,7 +1,15 @@
-# Implementation of Kruskal’s Minimum Spanning Tree Algorithm
+# Implementation of Kruskals Minimum Spanning Tree Algorithm
 
+# Part 3
 # 1. Sort all the edges in non-decreasing order of their weight.
 # 2. Pick the smallest edge. Check if it forms a cycle with the spanning tree formed so far. If cycle is not formed, include this edge. Else, discard it.
 # 3. Repeat step 2 until there are (V-1) edges in the spanning tree.
 
+import quicksort 
+
+# Edges from sample topology (undirected graph) in blackboard 
 edges = [('R1','R2',200),('R1','R3',500),('R1','R4',10),('R2','R4',70),('R2','R3',80),('R2','R5',90),('R3','R5',40),('R4','R5',200)]
+
+# Sorting edges
+quicksort.quick_sort(edges,0,len(edges)-1)
+
