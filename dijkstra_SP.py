09540c8c754c9f80197f4610a804e794cca03533
nickyik <nickyylau@gmail.com>
2020-05-04 18:37:07 -0500

    Added some comments

---
+++ dijkstra_SP.py
@@ -0,0 +1,77 @@
+# Implementation of Dijkstras Shortest Path Algorithm
+
+# Step 1) Create a set sptSet (shortest path tree set) that keeps track of vertices included in shortest path tree, i.e., whose minimum distance from source is calculated and finalized. Initially, this set is empty.
+# Step 2) For all vertices, assign a distance value to all other vertices in the input graph. Initialize all distance values as INFINITE. Assign distance value as 0 for the source vertex so that it is picked first.
+# Step 3) While sptSet doesn’t include all vertices
+#   a) Pick a vertex u which is not there in sptSet and has minimum distance value.
+#   b) Include u to sptSet.
+#   c) Update distance value of all adjacent vertices of u. To update the distance values, iterate through all adjacent vertices. 
+#      For every adjacent vertex v, if sum of distance value of u (from source) and weight of edge u-v, is less than the distance value of v, then update the distance value of v.
+
+import sys 
+
+class Graph:
+    def __init__(self,num_of_vertices):
+        self.vertices = num_of_vertices
+        # Step 2: Given map[i][k], for every map[i], it is a map of distance values  of all vertices depending on vertex i. 
+        self.map = [[0 for _ in range(num_of_vertices)] for _ in range(num_of_vertices)]
+    
+    # A function to find the vertex with minimum distance value, from the set of vertices not yet included in shortest path tree
+    def minimum_distance(self, sp_path, compression):
+        # Initilaize minimum distance for next node 
+        mini_dst = sys.maxsize
+        dest = -1
+        # Search not nearest vertex not in the short path tree 
+        for j in range(self.vertices):
+            if j not in sp_path and compression[j] < mini_dst:
+                mini_dst = compression[j]
+                dest = j
+        return dest
+
+    def dijkstra_sp(self,source):
+        # Step 1: Make solution set
+        sp_path = []
+        compression = [sys.maxsize] * self.vertices
+        compression[source] = 0
+        edges = []
+
+        # Step 3
+        while len(sp_path) != self.vertices:
+            # Pick the minimum distance vertex from set of vertices not processed yet
+            u = self.minimum_distance(sp_path, compression)
+            sp_path.append(u)
+
+            # Update all adjacent vertices of u 
+            for adj in range(self.vertices):
+                if self.map[u][adj] != 0 and adj not in sp_path:
+                    if compression[u] + self.map[u][adj] < compression[adj]: 
+                        compression[adj] = compression[u] + self.map[u][adj]
+                        
+                        exist = False
+                        for idx in range(len(edges)):
+                            if edges[idx][1] == adj:
+                                edges[idx] = (u,adj,compression[adj])
+                                exist = True
+                        if not exist:
+                            edges.append((u,adj,compression[adj]))
+        return edges
+
+graph = Graph(5)
+graph.map = [
+        [0, 200, 500, 10, 0], 
+        [200, 0, 80, 70, 90], 
+        [500, 80, 0, 0, 40], 
+        [10, 70, 0, 0, 200], 
+        [0, 90, 40, 200, 0]
+        ]; 
+
+orign_edges = []
+
+for x in range(graph.vertices): 
+    for y in range(x,graph.vertices):
+        if graph.map[x][y] != 0 and (x,y,graph.map[x][y]) not in orign_edges: 
+            orign_edges.append((x,y,graph.map[x][y]))
+
+print(f"The original graph has following edges: \n{orign_edges}\n")
+print(f"The shortest path tree using dijkstra's SP algorithm has following edges: \n{graph.dijkstra_sp(1)}\n")
+
---
+++ kurskal_MST.py
@@ -0,0 +1,76 @@
+# Implementation of Kruskals Minimum Spanning Tree Algorithm
+
+# Step 1. Sort all the edges in non-decreasing order of their weight.
+# Step 2. Pick the smallest edge. Check if it forms a cycle with the spanning tree formed so far. If cycle is not formed, include this edge. Else, discard it.
+# Step 3. Repeat step 2 until there are (V-1) edges in the spanning tree.
+
+class Graph: 
+    def __init__(self, num_of_vertices): 
+        self.V = num_of_vertices 
+        self.edges = [] 
+
+    # Adds an edge to graph
+    def add_edge(self,vertex_1,vertex_2,weight): 
+        self.edges.append([vertex_1,vertex_2,weight]) 
+
+    # Finds the very first parent/root
+    def find(self, parent, i):
+        # The very first parent should loop back to itself
+        if parent[i] == i: 
+            return i 
+        return self.find(parent, parent[i]) 
+
+    # Given two parents, compress the two into one
+    def union(self, parent, rank, vertex_1, vertex_2): 
+        super_parent_1 = self.find(parent, vertex_1) 
+        super_parent_2 = self.find(parent, vertex_2) 
+  
+        # Attach smaller rank tree under root of high rank tree
+        if rank[super_parent_1] < rank[super_parent_2]: 
+            parent[super_parent_1] = super_parent_2 
+        elif rank[super_parent_1] > rank[super_parent_2]: 
+            parent[super_parent_2] = super_parent_1 
+        # If ranks are same, then make one as root and increment its rank by one 
+        else : 
+            parent[super_parent_2] = super_parent_1 
+            rank[super_parent_1] += 1
+
+    def kruskals_mst(self): 
+        out = []
+        i = 0 
+        
+        # Step 1: sort by weight
+        self.edges =  sorted(self.edges,key=lambda e: e[2])
+        parent = [] ; rank = []
+
+        for node in range(self.V): 
+            parent.append(node) 
+            rank.append(0) 
+        
+        # Step 3: repeating step 2 until spanning tree has (V - 1) vertices
+        while len(out) < self.V -1:
+            vertex_1, vertex_2, weight =  self.edges[i] 
+            i = i + 1
+            root_of_vertex_1 = self.find(parent, vertex_1) 
+            root_of_vertex_2 = self.find(parent ,vertex_2)  
+
+            # Step 2: if root of vertex 1 is not equal to root of vertex 2, then there is no cycle
+            if root_of_vertex_1 != root_of_vertex_2: 
+                out.append([vertex_1,vertex_2,weight]) 
+                self.union(parent, rank, vertex_1, vertex_2)  
+        
+        return out
+
+graph = Graph(5) 
+graph.add_edge(0, 1, 200) 
+graph.add_edge(0, 2, 500) 
+graph.add_edge(0, 3, 10)
+graph.add_edge(1, 3, 70) 
+graph.add_edge(1, 2, 80) 
+graph.add_edge(1, 4, 90) 
+graph.add_edge(2, 4, 40) 
+graph.add_edge(3, 4, 200) 
+
+
+print(f"The original graph has following edges: \n{graph.edges}\n")
+print(f"The mininum spanning tree using kurskal's MST algorithm has following edges: \n{graph.kruskals_mst()}\n")
